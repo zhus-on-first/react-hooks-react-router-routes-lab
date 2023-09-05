65395dee73b35a7ec043f0b8de71082846ebe2e6
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-09-04 21:53:46 -0700

    code app, navbar, home, movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => {
+        return (
+          <div key={movie.title}>
+            <p>{movie.title}</p>
+            <p> {movie.time}</p>
+            <ul>
+              {movie.genres.map((genre, index) => {
+                return <li key={index}>{genre}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
